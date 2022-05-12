294c453b760cae841ad042889c34dec7f569d789
Jyotish Melwani <68257455+ProfessorJyo@users.noreply.github.com>
2022-05-12 16:52:05 -0400

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <div className='navbar-logo'>YO</div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
