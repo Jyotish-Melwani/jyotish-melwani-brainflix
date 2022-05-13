948bbfd9d74b88c437ab625d9c6da753f8ae9dc4
Jyotish Melwani <68257455+ProfessorJyo@users.noreply.github.com>
2022-05-12 20:23:01 -0400

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
+import logo from '../Assets/Logo/BrainFlix-logo.svg';
+import searchIcon from '../Assets/Icons/search.svg';
+import upload from '../Assets/Icons/upload.svg';
 
 function Navbar() {
   return (
     <div>
       <nav className='navbar'>
-        <div className='navbar-container'>
-          <div className='navbar-logo'>YO</div>
+        <div className='navbar__container'>
+          <div className='navbar__logo-container'>
+            <img className='header__logo' src={logo} alt='logo' />
+          </div>
         </div>
+        <div className='navbar__searchIcon-container'>
+          <img className='navbar__searchIcon-container--icon' src={searchIcon} alt='Search Icon' />
+          <h4 className='navbar__searchIcon-container--text'>Search</h4>
+        </div>
+        <div className='navbar__upload-container'>
+          <div className='navbar__upload-container-inner'>
+            <img className='navbar__upload-container-inner--icon' src={upload} alt='Upload Icon' />
+            <h4 className='navbar__upload-container-inner--text'>Upload</h4>
+          </div>
+        </div>
+        <div className='navbar__upload-container--image'></div>
       </nav>
     </div>
   )
