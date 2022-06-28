3e29121b10131bd4c38122bd03aa72323836562e
Jyotish Melwani <68257455+ProfessorJyo@users.noreply.github.com>
2022-06-27 21:43:29 -0400

    deleted master branch for fresh start

--- src/Helpers/Api.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "686acda6-06d0-41cd-930f-f91f224e0b1c"
-
-export const API_URL = "https://project-2-api.herokuapp.com/"
--- src/Helpers/Time.js
+++
@@ -1,8 +0,0 @@
-    // // functions
-    function getDate(dateVar){
-        return ('0' + (dateVar.getMonth() + 1)).slice(-2) + '/' + ('0' + dateVar.getDate()).slice(-2) + '/' + dateVar.getFullYear()
-    }
-
-    export default getDate
-
-
